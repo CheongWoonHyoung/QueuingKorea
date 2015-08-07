69ca867b5a8835d7b2760adf6b9ca956a8436dd0
Cheong WoonHyoung <cheongwh@GidiaMacBook.local>
2015-08-07 01:09:05 -0700

    restaurant info, comfirm, reservation info design updated

--- Queuing/app/src/main/java/com/example/cheongwh/queuing/RestaurantInfo.java
+++ Queuing/app/src/main/java/com/example/cheongwh/queuing/RestaurantInfo.java
@@ -1,17 +1,32 @@
 package com.example.cheongwh.queuing;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
-/**
- * Created by cheongwh on 2015. 8. 6..
- */
+
 public class RestaurantInfo extends Activity {
+
+    FrameLayout res_confirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_info);
 
-
+        res_confirm = (FrameLayout)findViewById(R.id.res_confirm);
+        res_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RestaurantInfo.this, ConfirmActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
+
